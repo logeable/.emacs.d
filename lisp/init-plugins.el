c9e38ce41b15ccdb109edd8461909ebdd1617a92
logeable <logeable@gmail.com>
2023-09-02 23:51:51 +0800

    add company

--- lisp/init-plugins.el
+++ lisp/init-plugins.el
@@ -80,5 +80,17 @@
   (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
   (dashboard-setup-startup-hook))
 
+(use-package company
+  :ensure t
+  :init
+  (global-company-mode)
+  :config
+  (setq company-minimum-prefix-length 1) ; 只需敲 1 个字母就开始进行自动补全
+  (setq company-tooltip-align-annotations t)
+  (setq company-idle-delay 0.0)
+  (setq company-show-numbers t) ;; 给选项编号 (按快捷键 M-1、M-2 等等来进行选择).
+  (setq company-selection-wrap-around t)
+  (setq company-transformers '(company-sort-by-occurrence)))
+
 
 (provide 'init-plugins)
