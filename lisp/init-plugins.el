6edd1848d676c7bb8a8585fae2c40280ca4b7351
logeable <logeable@gmail.com>
2023-09-02 23:09:35 +0800

    add embark

--- lisp/init-plugins.el
+++ lisp/init-plugins.el
@@ -26,6 +26,16 @@
 
 (use-package avy
   :ensure
+  :config
+  (defun avy-action-embark (pt)
+    (unwind-protect
+	(save-excursion
+          (goto-char pt)
+          (embark-act))
+      (select-window
+       (cdr (ring-ref avy-ring 0))))
+    t)
+  (setf (alist-get ?e avy-dispatch-alist) 'avy-action-embark)
   :bind
   (("C-j C-SPC" . avy-goto-char-timer)))
 
@@ -36,4 +46,31 @@
   :bind
   (:map minibuffer-local-map
 	("M-A" . marginalia-cycle)))
+
+(use-package embark
+  :ensure t
+
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc.  You may adjust the Eldoc
+  ;; strategy, if you want to see the documentation from multiple providers.
+  (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
 (provide 'init-plugins)
