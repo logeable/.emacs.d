1620d897921a0152ea9e2af483d70d8091fa1be7
logeable <logeable@gmail.com>
2023-09-03 01:05:10 +0800

    add rust

--- lisp/init-plugins.el
+++ lisp/init-plugins.el
@@ -153,4 +153,27 @@
 (use-package magit
   :ensure t)
 
+(use-package rust-mode
+  :ensure t
+  :functions dap-register-debug-template
+  :bind
+  ("C-c C-c" . rust-run)
+  :hook
+  (rust-mode . lsp-deferred)
+  :config
+  ;; debug
+  (require 'dap-gdb-lldb)
+  (dap-register-debug-template "Rust::LLDB Run Configuration"
+                               (list :type "lldb"
+				     :request "launch"
+			             :name "rust-lldb::Run"
+				     :gdbpath "rust-lldb"
+				     :target nil
+				     :cwd nil)))
+
+(use-package cargo
+  :ensure t
+  :hook
+  (rust-mode . cargo-minor-mode))
+
 (provide 'init-plugins)
