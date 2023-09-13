53935fd73e263d48db301a4e82d4d6cf4cd5fe77
logeable <logeable@gmail.com>
2023-09-13 10:53:15 +0800

    添加注释

--- early-init.el
+++ early-init.el
@@ -1,2 +1,7 @@
-;; -*- lexical-binding: t; -*-
+;;; early-init.el --- early init  -*- lexical-binding: t -*-
+;;; Commentary:
+
+;;; Code:
 (setq package-enable-at-startup nil)
+
+;;; early-init.el ends here
