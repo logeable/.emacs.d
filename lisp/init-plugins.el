f192af481e45f8657e9dba05cea56de199e785f0
logeable <logeable@gmail.com>
2023-09-13 10:43:10 +0800

    删除doom主题，配置org mode样式

--- lisp/init-plugins.el
+++ lisp/init-plugins.el
@@ -200,15 +200,6 @@
 
 (use-package all-the-icons)
 
-(use-package doom-themes
-  :ensure t
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold nil    ; if nil, bold is universally disabled
-	doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-dracula t)
-  (doom-themes-treemacs-config))
-
 (use-package org-bullets
   :ensure t
   :hook
