5c0ab21b79408de41bae11c3bcff9d457806a17d
logeable <logeable@gmail.com>
2024-08-15 17:38:29 +0800

    restructure

--- lisp/init-customs.el
+++ lisp/init-customs.el
@@ -1,12 +1,8 @@
 ;; -*- lexical-binding: t; -*-
 
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-
 (setq custom-file "~/.emacs.d/custom.el")
 (unless (file-exists-p custom-file)
   (write-region "" nil custom-file))
 (load custom-file)
 
-(require 'init-packages)
-(require 'init-customs)
-
+(provide 'init-customs)
---
+++ lisp/init-packages.el
@@ -0,0 +1,84 @@
+;; -*- lexical-binding: t; -*-
+
+(require 'package)
+(setq package-archives '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+                         ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-conjtents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+;; packages
+
+(use-package ivy
+  :config
+  (ivy-mode 1))
+
+(use-package counsel
+  :config
+  (counsel-mode 1)
+  (setq ivy-use-virtual-buffers t
+	ivy-count-format "(%d/%d)"))
+
+(use-package swiper
+  :bind
+  (("C-s" . 'swiper)))
+
+(use-package company
+  :hook
+  (prog-mode . global-company-mode)
+  :config
+  (setq company-minimum-prefix-length 1))
+
+(use-package which-key
+  :config
+  (which-key-mode))
+
+(use-package projectile
+  :config
+;  (setq projectile-cache-file (expand-file-name ".cache/projectile.cache" user-emacs-directory))
+  (projectile-mode 1)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map))
+
+(use-package magit)
+
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :config
+  (exec-path-from-shell-initialize))
+
+(use-package vterm)
+
+(use-package evil
+  :config
+  (evil-mode 1))
+
+(use-package go-mode
+  :init
+  :config
+  (setq gofmt-command "goimports")
+  (add-hook 'go-mode-hook
+	    (lambda ()
+	      (add-hook 'before-save-hook 'gofmt-before-save))))
+
+(use-package rust-mode
+  :config
+  (setq rust-format-on-save t))
+
+(use-package lsp-mode
+  :hook
+  ((go-mode . lsp)
+   (rust-mode . lsp)
+   (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package lsp-ui
+  :commands lsp-ui-mode)
+
+(provide 'init-packages)
